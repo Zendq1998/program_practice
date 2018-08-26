39aec39b486000e66b977b722e2c2f66f95bcc8b
1 <1305233756@qq.com>
2018-08-26 10:02:02 +0800

    feat: add leetcode 14

---
+++ leetCode/14-Longest-Common-Prefix/main.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+function minLenth(totoalMin, currentStr) {
+  return totoalMin <= currentStr.length ? totoalMin : currentStr.length;
+}
+
+function isSame(i, num) {
+  return function(temp) {
+    return num === temp[i];
+  }
+}
+
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) {
+      return ""
+    }
+    let result = "";
+    const minL = strs.reduce(minLenth,strs[0].length);
+    for(let i=0;i<minL;i++) {
+      let num = strs[0][i]
+      if (strs.every(isSame(i, num))) {
+        result += num;
+      }
+      else break;
+    }
+    return result;
+};
+
+
+longestCommonPrefix(["a"])
