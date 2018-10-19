120540b96a159ce98fa481e5247a1394d9552ca6
Zendq1998 <1305233756@qq.com>
2018-10-19 09:02:40 +0800

    feat: add leetcode 23&24

---
+++ 23.merge-k-sorted-lists.js
@@ -0,0 +1,41 @@
+/*
+ * [23] Merge k Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-k-sorted-lists/description/
+ *
+ * algorithms
+ * Hard (30.57%)
+ * Total Accepted:    280.2K
+ * Total Submissions: 916K
+ * Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+ *
+ * Merge k sorted linked lists and return it as one sorted list. Analyze and
+ * describe its complexity.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * Output: 1->1->2->3->4->4->5->6
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    
+};
---
+++ leetCode/024.Swap-Nodes-in-Pairs/main.js
@@ -0,0 +1,52 @@
+/*
+ * [24] Swap Nodes in Pairs
+ *
+ * https://leetcode.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (41.15%)
+ * Total Accepted:    246.2K
+ * Total Submissions: 598K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * 
+ * Example:
+ * 
+ * 
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ * 
+ * Note:
+ * 
+ * 
+ * Your algorithm should use only constant extra space.
+ * You may not modify the values in the list's nodes, only nodes itself may be
+ * changed.
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+  var Head = {next: head}
+  var i = Head
+  var j
+  var temp
+  while (j = i.next && j.next) {
+    temp = j.next
+    j.next = temp.next
+    temp.next = j
+    i.next = temp
+    i = j
+  }
+  return Head.next
+};
